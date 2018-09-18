995293f67499ea89ab0cb1d854c15156e1958f34
Dan Walmsley <dan@walms.co.uk>
2018-09-18 18:40:27 +0100

    add managed interface implementations

---
+++ src/Avalonia.Native/DragSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avalonia.Input;
+using Avalonia.Input.Platform;
+
+namespace Avalonia.Native
+{
+    public class DragSource : IPlatformDragSource
+    {
+        public Task<DragDropEffects> DoDragDrop(IDataObject data, DragDropEffects allowedEffects)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Avalonia.Native/DraggingInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Avalonia.Input;
+
+namespace Avalonia.Native
+{
+    public class DraggingInfo : IDataObject
+    {
+        public bool Contains(string dataFormat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Get(string dataFormat)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetDataFormats()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetFileNames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
